2c088e7677ec0d4f8dd5d029e54ca29bbb86db31
Ilya Victorov <vivgman@bk.ru>
2019-06-14 12:55:37 +0300

    Add keywords

--- src/stringify.js
+++ src/stringify.js
@@ -8,16 +8,34 @@
  * @example
  * import { stringify } from 'query-params-helpers';
  *
- * stringify({ foo: 'bar' }); 
+ * stringify({ 
+ *   foo: 'bar' 
+ * }); 
  * // '?foo=bar'
  * 
- * stringify({ foo: [1, 2] }); 
+ * stringify({ 
+ *   foo: [1, 2] 
+ * }); 
  * // '?foo[]=1&foo[]=2'
  * 
- * stringify({ foo: 1, bar: { x: 1, y: 2 } }); 
+ * stringify({ 
+ *   foo: 1, 
+ *   bar: { 
+ *     x: 1, 
+ *     y: 2
+ *   } 
+ * }); 
  * // '?foo=1&bar[x]=2&bar[y]=3'
  * 
- * stringify({ foo: [{ x: 10, y: 20 }, { x: 30, y: 40 }] }); 
+ * stringify({ 
+ *   foo: [{ 
+ *     x: 10, 
+ *     y: 20 
+ *   }, { 
+ *     x: 30, 
+ *     y: 40 
+ *   }] 
+ * }); 
  * // '?foo[0][x]=10&foo[0][y]=20&foo[1][x]=30&foo[1][y]=40'
  */
 export function stringify(object, url = '') {
